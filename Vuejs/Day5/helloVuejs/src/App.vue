0c0334e6859e68ec050a277fb19a8eee4fd7b04d
drodevilosun <drodevilosun@gmail.com>
2020-01-01 17:41:19 +0700

    Add babel and webpack to support create  website

---
+++ Vuejs/Day5/helloVuejs/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+/* 
+App
+  Header
+  ListProduct
+    Product
+    Product
+    Product
+    Product
+    Product
+  Footer
+*/
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello VueJs'
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
