a9fc1a81cfb9de6f4615ca41950298118f4cbb28
drodevilosun <drodevilosun@gmail.com>
2020-06-01 22:49:36 +0700

    Hanle for activePlayer
    - Add Current score for 2 players.
    - Use fucntion SetTimout() to synchronyze with function dis
    play activePlayer() (đồng bộ với việc hiển thị dấu chấm khi tới lượt người chơi. Nếu không có hàm này alert() sẽ hiện thị ra sớm hơn dấu chỉ thị người chơi => mất đồng bộ) = delay 10ms để hiển thị dấu chỉ thị người chơi ra trước.
    - Use grave accent in javascript (`) (dấu huyền)
    (`) denotes a template string. Template strings have some abilities that normal strings do not. Most importantly, you get interpolation:
    Ex: refer https://stackoverflow.com/questions/33679732/difference-if-there-is-any-between-and-in-javascript
    <pre>
      var value = 123;
      console.log('test ${value}') //=> test ${value}
      console.log(`test ${value}`) //=> test 123
    </pre>
    
    And multiline strings:
    <pre>
      console.log('test
      test')
      // Syntax error
    
      console.log(`test
      test`)
      // test
      // test
    </pre>

--- Vuejs/Day6/Game/src/App.vue
+++ Vuejs/Day6/Game/src/App.vue
@@ -51,43 +51,74 @@ export default {
   methods: {
     handleConfirm()
     {
-      this.isPlaying = true;
-      this.isOpenPopup = false;
-      this.activePlayer = 0;
-      this.scoresPlayer = [0, 0];
-      console.log('Confirm in App');
-      this.currentScore = 0;
-      this.dices = [1, 1];
+        this.isPlaying = true;
+        this.isOpenPopup = false;
+        this.activePlayer = 0;
+        this.scoresPlayer = [0, 0];
+        console.log('Confirm in App');
+        this.currentScore = 0;
+        this.dices = [1, 1];
     },
     handleNewGame() 
     {
-      console.log('handleNewGame App.vue');
-      //Hiển thị Popup -> Show luật chơi
-      this.isOpenPopup = true;
+        console.log('handleNewGame App.vue');
+        //Hiển thị Popup -> Show luật chơi
+        this.isOpenPopup = true;
 
+    },
+    nextPlalyer()
+    {
+        // Đảo activePlayer
+        this.activePlayer = this.activePlayer === 0 ? 1 : 0;
+        this.currentScore = 0;
     },
     handleRollDiceApp()
     {
-      console.log("handleRollDice App.vue");
-      if (this.isPlaying)
-      {
+        console.log("handleRollDice App.vue");
+        if (this.isPlaying)
+        {
         //roll dice
         //Math.random(): 0->1
         /*
-          0 <= x <= 1
-          0*6 <= x*6 <= 1*6
-          0 <= y= x*6 <= 6
+            0 <= x <= 1
+            0*6 <= x*6 <= 1*6
+            0 <= y= x*6 <= 6
         */
-       var dice1 = Math.floor(Math.random() * 6) + 1;
-       var dice2 = Math.floor(Math.random() * 6) + 1;
-       console.log(dice1, dice2);
-       this.dices = [dice1, dice2];
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+        
+        this.dices = [dice1, dice2];
+        console.log(dice1, dice2);
 
-      }
-      else
-      {
+        if (dice1 === 1 || dice2 ===1)
+        {
+            // Đổi lượt chơi
+            let activePlayer = this.activePlayer;
+            // Use arrow fucntion
+            setTimeout(() => {
+                console.log(this); //'this' ở đây thuộc thằng Vue
+                alert(`Người chơi Player ${this.activePlayer + 1} đã quay trúng số 1. Next Player`);
+            }, 10);
+            // Use function()
+            // setTimeout(function() {
+            //     console.log(this); //'this' ở đây thuộc Window
+            //     alert(`Người chơi Player ${activePlayer + 1} đã quay trúng số 1. Next Player`);
+            // }, 10);
+            
+            this.nextPlalyer();
+            // Reset điểm tạm thời về 0
+        }
+        else
+        {
+            // Cộng dồn vảo điểm tạm thời cho ngưởi chơi
+            this.currentScore = this.currentScore + dice1 + dice2;
+        }
+
+        }
+        else
+        {
         alert("Vui lòng nhấp vào nút New Game");
-      }
+        }
     }
   }
 }
